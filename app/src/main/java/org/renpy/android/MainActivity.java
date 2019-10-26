3ff1eab716000f7e130c2fd630fd48ee88eb096e
CkaiGrac <1043932993@qq.com>
2019-10-26 13:11:39 +0800

    初始化工程

---
+++ app/src/main/java/org/renpy/android/MainActivity.java
@@ -0,0 +1,13 @@
+package org.renpy.android;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
